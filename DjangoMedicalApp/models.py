47e6103f89f3e31c6eea42ccf7f4d50232ea23ef
DEVELOPER <developer@localhost.localdomain>
2023-07-07 09:55:36 +0300

    Changes

--- DjangoMedicalApp/models.py
+++ DjangoMedicalApp/models.py
@@ -41,7 +41,7 @@ class MedicalDetails(models.Model):
     added_on=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
 
-class Employee(models.model):
+class Employee(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
     joining_date=models.DateField()
@@ -90,11 +90,11 @@ class CustomerRequest(models.Model):
     objects=models.Manager()
 
 class CompanyAccount(models.Model):
-    choices=("Debit", "Credit",)
+    choices=((1,"Debit"),(2,"Credit"))
     id=models.AutoField(primary_key=True)
     company_id=models.ForeignKey(Company,on_delete=models.CASCADE)
-    transaction_type=models.Choices(choices)
-    transaction_amount=models.DecimalField(max_digits=255)
+    transaction_type=models.CharField(choices=choices,max_length=255)
+    transaction_amount=models.CharField(max_length=255)
     transaction_date=models.DateField()
     payment_mode=models.CharField(max_length=255)
     added_on=models.DateTimeField
