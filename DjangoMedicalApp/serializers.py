1626540af904aef8055f16ba10565703659b0cc3
tulusdeveloper <tulusdeveloper@gmail.com>
2023-07-12 15:22:06 +0300

    All Serializers

--- DjangoMedicalApp/serializers.py
+++ DjangoMedicalApp/serializers.py
@@ -1,6 +1,7 @@
+# from rest_framework import response
 from rest_framework import serializers
 
-from DjangoMedicalApp.models import Company, CompanyBank
+from DjangoMedicalApp.models import Bill, BillDetails, Company, CompanyAccount, CompanyBank, Customer, CustomerRequest, Employee, EmployeeBank, EmployeeSalary, MedicalDetails, Medicine
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,7 +17,89 @@ class CompanyBankSerializer(serializers.ModelSerializer):
         response=super().to_representation(instance)
         response['company']=CompanySerializer(instance.company_id).data
         return response
+    
+class MedicineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medicine
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['company']=CompanySerializer(instance.company_id).data
+        return response
+
+class MedicalDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MedicalDetails
+        fields="__all__"
+    
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['medicine']=MedicineSerializer(instance.medicine_id).data
+        return response
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields="__all__"
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields="__all__"
         
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bill
+        fields="__all__"
+    
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['customer']=CustomerSerializer(instance.customer_id).data
+        return response
 
+class EmployeeSalarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployeeSalary
+        fields="__all__"
 
-       
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['employee']=EmployeeSerializer(instance.Employee_id).data
+        return response
+
+class BillDetailsSalarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BillDetails
+        fields="__all__"
+    
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['bill']=BillSerializer(instance.bill_id).data
+        return response
+
+class CustomerRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerRequest
+        fields="__all__"
+
+class CompanyAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyAccount
+        fields="__all__"
+
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['company']=CompanySerializer(instance.company_id).data
+        return response
+
+class EmployeeBankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployeeBank
+        fields="__all__"
+    
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['employee']=EmployeeSerializer(instance.employee_id).data
+        return response
+
