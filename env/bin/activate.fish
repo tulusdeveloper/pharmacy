d9aa1de8825e5127aa960df6bf70baf1d8d3967f
tulusdeveloper <tulusdeveloper@gmail.com>
2023-11-16 13:34:20 +0300

    change of env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/linux-dev/python/pharmacy/env"
+set -gx VIRTUAL_ENV "/home/devlp/python/pharmacy/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
