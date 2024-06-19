486698419ca14a63a8f3d764c9a0cf0fad796f12
tulusdeveloper <tulusdeveloper@gmail.com>
2024-06-19 13:20:36 +0300

    updates

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e_=i-*3*_s5!4#yke4#fuwscln%*)mpm1@$23)mp5=9gd6y4zz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.100.20']
 
 
 # Application definition
